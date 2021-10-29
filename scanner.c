69f02d3e467c9b8f24b91a2497cbf832fc4416ef
jscblack <jscblack@163.com>
2021-10-29 22:08:04 +0800

    finish part of operators

--- scanner.c
+++ scanner.c
@@ -1,6 +1,6 @@
 /*
  * @Author       : Gehrychiang
- * @LastEditTime : 2021-10-29 18:52:45
+ * @LastEditTime : 2021-10-29 22:06:08
  * @Website      : www.yilantingfeng.site
  * @E-mail       : gehrychiang@aliyun.com
  * @ProbTitle    : (记得补充题目标题)
@@ -60,6 +60,7 @@ number(oct,dec,hex)
 */
 
 #include <stdio.h>
+#include <string.h>
 #define keyword_count 24
 char reserved_keyword[][20] = {"auto", "break", "case", "char", "continue",
                                "const", "default", "do", "double", "else",
@@ -70,12 +71,18 @@ char reserved_keyword[][20] = {"auto", "break", "case", "char", "continue",
 typedef enum
 {
     Start_state,
+    In_ID_state,
+    ID_end_state,
+
+    plus_state,
+    minus_state,
 
     In_comment_state,
     In_dec_state,
     In_oct_state,
     In_hex_state,
 
+    Err_state,
     End_state,
 } state_type;
 
@@ -92,12 +99,32 @@ typedef enum
     single_delimiter,
     algo_operator,
     self_operator,
+    compound_assignment_operator,
     single_relational_operator,
     double_relational_operator,
     char_const,
     string_const,
     eof
 } word_type;
+
+char friendly_wordtype[][105] = {
+    "error",
+    "keyword",
+    "identifier",
+    "dec_integer",
+    "oct_integer",
+    "hex_integer",
+    "real_number",
+    "comment",
+    "single_delimiter",
+    "algo_operator",
+    "self_operator",
+    "compound_assignment_operator",
+    "single_relational_operator",
+    "double_relational_operator",
+    "char_const",
+    "string_const",
+    "eof"};
 
 typedef enum
 {
@@ -138,6 +165,7 @@ typedef enum
 
 word_type get_word_type(char *str)
 {
+    // printf("debug::%s\n\n",str);
     int found = 0;
     for (int i = 0; i < keyword_count; i++)
     {
@@ -155,9 +183,9 @@ word_type get_word_type(char *str)
 }
 character_type get_character_type(char ch, state_type cur_state)
 {
-    if (cur_state == &&(ch == 'e' || ch == 'E')) //在十进制整数下处理指数
+    if (cur_state == In_dec_state && (ch == 'e' || ch == 'E')) //在十进制整数下处理指数
         return exponent;
-    else if (cur_state == &&((ch >= 'a' && ch <= 'e') || (ch >= 'A' && ch <= 'E'))) //在十六进制下处理a-e
+    else if (cur_state == In_hex_state && ((ch >= 'a' && ch <= 'e') || (ch >= 'A' && ch <= 'E'))) //在十六进制下处理a-e
         return hex_digit;
     else if (ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z')
         return letter;
@@ -211,8 +239,146 @@ character_type get_character_type(char ch, state_type cur_state)
         return unknown;
     return 0;
 }
+character_type get_next_character_type(char *straddr, int strlength, int cur_pos, state_type cur_state)
+{
+    if (cur_pos >= strlength)
+        return eof;
+    else
+        return get_character_type(straddr[cur_pos], cur_state);
+}
+void scopy_word(char *tmpstr, char *straddr, int l, int r)
+{
+    memset(tmpstr, sizeof(tmpstr), 0);
+    for (int i = l; i < r; i++)
+    {
+        tmpstr[i - l] = straddr[i];
+    }
+}
 
+word_type RecogniteWordByDFA(char *straddr, int strlength, int *start_pos)
+{
+
+    state_type cur_state = Start_state;
+    word_type wordtype = error;
+
+    int cur_pos = *start_pos;
+    char tmpstr[256];
+
+    while (cur_state != End_state)
+    {
+        switch (cur_state)
+        {
+        case Start_state:
+            switch (get_next_character_type(straddr, strlength, cur_pos, cur_state))
+            {
+            case letter:
+                cur_state = In_ID_state;
+                break;
+            case underline:
+                cur_state = In_ID_state;
+                break;
+            case plus:
+                cur_state = plus_state;
+                break;
+            case minus:
+                cur_state = minus_state;
+                break;
+
+            default:
+                cur_state = Err_state;
+                break;
+            }
+            cur_pos++;
+            break;
+
+        case In_ID_state:
+            if (get_next_character_type(straddr, strlength, cur_pos, cur_state) != letter && get_next_character_type(straddr, strlength, cur_pos, cur_state) != digit && get_next_character_type(straddr, strlength, cur_pos, cur_state) != underline)
+            {
+                cur_state = ID_end_state;
+            }
+            else
+            {
+                cur_pos++;
+                cur_state = In_ID_state;
+            }
+
+            break;
+
+        case ID_end_state:
+            scopy_word(tmpstr, straddr, *start_pos, cur_pos);
+            wordtype = get_word_type(tmpstr);
+            cur_state = End_state;
+            break;
+
+        case plus_state:
+            if (get_next_character_type(straddr, strlength, cur_pos, cur_state) == plus)
+            {
+                wordtype = self_operator;
+                cur_state = End_state;
+                cur_pos++;
+            }
+            else if (get_next_character_type(straddr, strlength, cur_pos, cur_state) == eq)
+            {
+                wordtype = compound_assignment_operator;
+                cur_state = End_state;
+                cur_pos++;
+            }
+            else
+            {
+                wordtype = algo_operator;
+                cur_state = End_state;
+            }
+            scopy_word(tmpstr, straddr, *start_pos, cur_pos);
+
+            break;
+
+        case minus_state:
+            if (get_next_character_type(straddr, strlength, cur_pos, cur_state) == minus)
+            {
+                wordtype = self_operator;
+                cur_state = End_state;
+                cur_pos++;
+            }
+            else if (get_next_character_type(straddr, strlength, cur_pos, cur_state) == eq)
+            {
+                wordtype = compound_assignment_operator;
+                cur_state = End_state;
+                cur_pos++;
+            }
+            else
+            {
+                wordtype = algo_operator;
+                cur_state = End_state;
+            }
+            scopy_word(tmpstr, straddr, *start_pos, cur_pos);
+
+            break;
+        case Err_state:
+            scopy_word(tmpstr, straddr, *start_pos, cur_pos);
+            wordtype = error;
+            cur_state = End_state;
+            break;
+        default:
+            wordtype = error;
+            cur_state = End_state;
+            break;
+        }
+    }
+    *start_pos = cur_pos;
+    printf("%s ", tmpstr);
+    return wordtype;
+}
 int main()
 {
     //recognize id
+    char input_str[10005];
+    gets(input_str);
+    int curpos = 0;
+    while (curpos < strlen(input_str))
+    {
+        word_type res = RecogniteWordByDFA(input_str, strlen(input_str), &curpos);
+        printf("%s\n", friendly_wordtype[res]);
+    }
+
+    return 0;
 }
